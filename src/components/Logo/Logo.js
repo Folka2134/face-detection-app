4731a316e4280fff707e778ee09314aedad54a46
folka2134 <folka2134@gmail.com>
2021-11-09 12:39:18 +0000

    updated logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,13 @@
 import React from "react";
-import Tilt from "react-tilt";
-import brain from "./brain-ver5.jpg";
+// import Tilt from "react-tilt";
+import brain from "./brain.png";
 
 const Logo = () => {
   return (
-    <div className="ml5">
-      <Tilt
-        className="Tilt"
-        options={{ max: 45 }}
-        style={{ height: 150, width: 150 }}
-      >
-        <div className="Tilt-inner">
-          <img alt="" src={brain} />
-        </div>
-      </Tilt>
+    <div className="logo-container ma4 mt0 pa3">
+      <div className="Tilt-inner">
+        <img alt="" src={brain} />
+      </div>
     </div>
   );
 };
