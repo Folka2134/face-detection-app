9d0aaff6f6e1321610ca1ac9ed668183b2593c4a
folka2134 <folka2134@gmail.com>
2021-12-13 16:24:08 +0000

    Updates

---
+++ precache-manifest.98cdf0340c0c61d8b2fac0590065ce5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585f31bc4fa5fd7715e111e8da06076f",
+    "url": "/face-detection-app/index.html"
+  },
+  {
+    "revision": "52078a81568a2080ee18",
+    "url": "/face-detection-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e6472bb232d57f7815ff",
+    "url": "/face-detection-app/static/css/main.71e95fca.chunk.css"
+  },
+  {
+    "revision": "52078a81568a2080ee18",
+    "url": "/face-detection-app/static/js/2.59f6e860.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/face-detection-app/static/js/2.59f6e860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6472bb232d57f7815ff",
+    "url": "/face-detection-app/static/js/main.19e944f4.chunk.js"
+  },
+  {
+    "revision": "ae27e0eae149d55e02c0",
+    "url": "/face-detection-app/static/js/runtime-main.aea30acc.js"
+  },
+  {
+    "revision": "fee6bc469fbbf5acd24244e3ac3c1abc",
+    "url": "/face-detection-app/static/media/brain.fee6bc46.png"
+  }
+]);
